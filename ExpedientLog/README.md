966d735ba3112b79d1a0237a186ee64437a62011
Patson Tembo <tembopatson5@gmail.com>
2026-06-09 15:26:43 +0200

    Delete ExpedientLog directory

--- ExpedientLog/README.md
+++
@@ -1,2 +0,0 @@
-# ExpedientLog
-Fast, reliable and smart way of tracking workers' tasks, and better way of proving the competence of each team member.
--- ExpedientLog/api/get_dashboard_data.php
+++
@@ -1,5 +0,0 @@
-<?php
-// FILE: api/get_dashboard_data.php
-
-require_once 'config.php'; // Adjust path to your config.php
-header('Content-Type: application/json');
